6ba751044d9c09f1270b8f5a1e09cc8d03312e6f
ricky0601 <ricky2425@naver.com>
2024-01-16 17:28:55 +0900

    0116 modify

--- js/gProject/script.js
+++ js/gProject/script.js
@@ -12,3 +12,42 @@ const enterKey = (event) => {
 searchInput.addEventListener("keypress", (event) => {
     enterKey(event);
 });
+
+let nowZoom = 100;
+//화면 크기 축소
+function zoomOut(){
+    nowZoom = nowZoom - 10;
+
+    // 화면크기 최대 축소율 70%
+    if (nowZoom <= 70){
+        nowZoom = 70;
+    }
+    zooms();
+}
+
+//화면 크기 확대
+function zoomIn(){
+    nowZoom+=20;
+
+    //화면크기 최대 확대율 200%
+    if (nowZoom >= 200){
+        nowZoom = 200;
+    }
+    zooms();
+}
+
+//원래 화면 크기 되돌리기
+function zoomReset(){
+    nowZoom = 100;
+    zooms();
+}
+
+function zooms(){
+    document.getElementById("main_frame").style.transform = "scale("+nowZoom + "%)";
+    if(nowZoom == 70){
+        alert("더 이상 축소 X");
+    }
+    if(nowZoom == 200){
+        alert("더 이상 확대 X");
+    }
+}
