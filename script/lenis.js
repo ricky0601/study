615cee344c9d0c81445e49c3ca5e6bb9fc5ee63a
ricky0601 <ricky2425@naver.com>
2024-06-11 15:44:29 +0900

    Add top button
    0611

--- script/lenis.js
+++ script/lenis.js
@@ -10,3 +10,22 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf);
+
+$(document).ready(function() { 
+
+    // Top 버튼 특정 스크롤높이에서만 보이기 / 숨기기
+    $(window).scroll(function(){
+    	if($(this).scrollTop() > 100){
+        	$('#top-btn').fadeIn();
+        }else{
+        	$('#top-btn').fadeOut();
+        }
+    });
+    
+    // Top 버튼 클릭시 페이지 상단으로 이동
+   	$('#top-btn').click(function(){
+    	$('html, body').animate({scrollTop : 0}, 800);
+        return false;
+    });
+    
+ });
